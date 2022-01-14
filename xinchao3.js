6788d77bd0808f2add759fd3fd45f17140d9bdda
dinhnehihi <97022359+dinhnehihi@users.noreply.github.com>
2022-01-14 09:58:28 +0700

    hmmmm

---
+++ xinchao3.js
@@ -0,0 +1,9 @@
+let a = prompt('Nhập a:');
+let b = prompt('Nhập b:');
+let check = a % b;
+if(check === 0) {
+  document.write('a là bội số của b');
+}
+else {
+  document.write('a không phải là bội số của b');
+}
