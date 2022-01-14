c93bedd32989bb45ebbea6106995a503e01c0ff8
dinhnehihi <97022359+dinhnehihi@users.noreply.github.com>
2022-01-14 09:48:30 +0700

    hi

---
+++ xinchao2.js
@@ -0,0 +1,5 @@
+let width = prompt('Nhập chiều rộng: ');
+let length = prompt('Nhập chiều dài: ');
+let area = width * length;
+document.write('Diện tích hình chữ nhật là: ' + area); //Cách 1
+// alert('Diện tích của hình chữ nhật là: ' + area); //Cách 2
